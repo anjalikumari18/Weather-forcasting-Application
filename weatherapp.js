9d48d0f69e9db4354a76f2d9a1cb3bbfea4a007d
anjalikumari18 <142581224+anjalikumari18@users.noreply.github.com>
2023-11-04 17:55:25 +0530

    Add files via upload

---
+++ weatherapp.js
@@ -0,0 +1,23 @@
+let key='c9829dcceb00466f3b2f417cbd099d92';
+let city=document.getElementById('city');
+let desc=document.getElementById('desc');
+let windspeed=document.getElementById('windspeed');
+let hum=document.getElementById('hum');
+let temp=document.getElementById('temp');
+
+function fetchData(){
+	let cityname=document.getElementById('cityname').value;
+	let api=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${key}`
+
+	fetch(api)
+	.then(repsonse=>repsonse.json())
+	.then(data=>{
+		city.innerHTML=data.name;
+		desc.innerHTML=data.weather[0].description;
+		windspeed.innerHTML=data.wind.speed;
+		hum.innerHTML=data.main.humidity;
+        temp.innerHTML=data.main.temp-273;
+
+
+	})
+}
